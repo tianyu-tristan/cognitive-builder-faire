5914c611753dfd05efa79e6a66fb60f3e882ffd3
tristan <tianyu.tristan@gmail.com>
2017-04-22 10:50:30 -0700

    use yaml to load credentials

--- python/text_to_speech/tts.py
+++ python/text_to_speech/tts.py
@@ -1,24 +1,23 @@
 import os
 import json
-from os.path import join, dirname
+from os.path import join, dirname, expanduser
 from dotenv import load_dotenv
 from watson_developer_cloud import TextToSpeechV1
+import yaml
 
 def convert_to_audio(tts, text):
   with open('output.wav', 'wb') as audio_file:
     audio_file.write(
       tts.synthesize(
-        text, 
+        text,
         accept="audio/wav",
         voice="en-US_AllisonVoice"))
 
 def main():
-  dotenv_path = join(dirname(__file__), '.env')
-  load_dotenv(dotenv_path)
-  
+  credentials = yaml.load(open(expanduser('~/.ssh/api_cred.yml')))['watson']
   tts = TextToSpeechV1(
-    username=os.environ.get("TTS_USERNAME"),
-    password=os.environ.get("TTS_PASSWORD"),
+    username=credentials['TTS_USERNAME'],
+    password=credentials['TTS_PASSWORD'],
     x_watson_learning_opt_out=True)  # Optional flag
 
   text = "Watson loves galvanize and Seattle"
